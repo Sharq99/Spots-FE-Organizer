3972d469b45032b169d674b54c98d062aa449bcf
aa0241 <77753344+aa0241@users.noreply.github.com>
2022-12-28 00:14:22 +0300

    Edited instance

--- src/stores/instance.js
+++ src/stores/instance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.NODE_ENV === "production" ? "/" : "http://192.168.0.80:8000";
+  process.env.NODE_ENV === "production"
+    ? "http://destkw.com"
+    : "http://192.168.0.80:8000";
 
 const instance = axios.create({
   baseURL: baseURL,
